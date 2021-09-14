6cea3f686d24305091fbe0c9acefadd105c3da1b
Aliya Shakirova <boogie_woogie8@inbox.ru>
2021-09-14 15:28:41 +0300

    add factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+  var result = 1;
+  while (n) {
+    if (n == 0 || n == 1) break;
+    result = result * n;
+    n = n - 1;
+  }
+  console.log(result);
+  return result;
 }
+
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(3)); // 6
+console.log(factorial(5)); // 120
