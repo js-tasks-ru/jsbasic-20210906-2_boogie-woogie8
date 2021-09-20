80b75b2c1ebb8f7346d4216ad8c0319b6a79fd15
Aliya Shakirova <boogie_woogie8@inbox.ru>
2021-09-20 22:54:59 +0300

    add 4 task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
+let inputData = "1 и -5.8 или 10 хотя 34 + -5.3 и 73";
+
 function getMinMax(str) {
-  // ваш код...
+  let strSplit = str.split(" ");
+  let strMap = strSplit.map((item) => +item);
+  let newArray = strMap.filter(Boolean);
+  console.log(newArray);
+  return {
+    min: Math.min.apply(Math, newArray),
+    max: Math.max.apply(Math, newArray),
+  };
 }
