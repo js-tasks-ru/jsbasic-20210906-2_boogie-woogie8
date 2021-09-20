443c86f2b0e9a116fb56e185459ae4393a856da9
Aliya Shakirova <boogie_woogie8@inbox.ru>
2021-09-20 23:24:01 +0300

    fix module 3

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,7 +1,10 @@
 function namify(users) {
+  let names = users.map((user) => user.name);
+  /*
   let names = [];
   users.forEach(function (user) {
     names.push(user.name);
   });
+  */
   return names;
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,10 +1,7 @@
-let inputData = "1 и -5.8 или 10 хотя 34 + -5.3 и 73";
-
 function getMinMax(str) {
   let strSplit = str.split(" ");
   let strMap = strSplit.map((item) => +item);
   let newArray = strMap.filter(Boolean);
-  console.log(newArray);
   return {
     min: Math.min.apply(Math, newArray),
     max: Math.max.apply(Math, newArray),
