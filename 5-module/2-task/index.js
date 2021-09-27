1b1305323fd4db2ddd62c5e7c653ce686a830282
Aliya Shakirova <boogie_woogie8@inbox.ru>
2021-09-27 20:16:56 +0300

    add module-5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,15 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector(".toggle-text-button");
+  let text = document.querySelector("#text");
+  button.addEventListener("click", function () {
+    console.log(text.hidden);
+    if (text.hidden === false) {
+      text.hidden = true;
+      console.log(text.hidden);
+    } else {
+      text.hidden = false;
+    }
+  });
 }
+
+toggleText();
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  let buttonRight = document.querySelector(".carousel__arrow_right");
+  let buttonLeft = document.querySelector(".carousel__arrow_left");
+  buttonLeft.style.display = "none";
+  let carousel = document.querySelector(".carousel__inner");
+  let slide = document.querySelector(".carousel__slide");
+  let currentWidth = slide.offsetWidth;
+  let slideWidth = currentWidth;
+  buttonRight.addEventListener("click", function () {
+    buttonLeft.style.display = "block";
+    carousel.style.transform = "translateX(-" + slideWidth + "px";
+    slideWidth += currentWidth;
+    console.log(slideWidth);
+    if (slideWidth === 3952) {
+      buttonRight.style.display = "none";
+      slideWidth = 2964;
+    }
+  });
+  buttonLeft.addEventListener("click", function () {
+    buttonRight.style.display = "block";
+    slideWidth -= currentWidth;
+    if (slideWidth <= 2964 || slideWidth >= 0) {
+      carousel.style.transform = "translateX(-" + slideWidth + "px";
+      console.log(slideWidth);
+    }
+    if (slideWidth === 0) {
+      buttonLeft.style.display = "none";
+      slideWidth = 988;
+    }
+  });
 }
+
+initCarousel();
