10d68b0e8cb6da165ff6b4c394e1baa940c63249
Aliya Shakirova <boogie_woogie8@inbox.ru>
2021-10-05 20:55:55 +0300

    fix module 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -5,9 +5,35 @@ function initCarousel() {
   //buttonRight.style.display = "none";
   let carousel = document.querySelector(".carousel__inner");
   let slide = document.querySelector(".carousel__slide");
+  let currentStep = 0;
   let currentWidth = slide.offsetWidth;
   let slideWidth = currentWidth;
 
+  buttonRight.addEventListener("click", function () {
+    currentStep += 1;
+    buttonLeft.style.display = "block";
+    carousel.style.transform = `translateX(-${slideWidth}px)`;
+    slideWidth += currentWidth;
+    if (slideWidth === currentWidth * 4) {
+      buttonRight.style.display = "none";
+      slideWidth = currentWidth * 3;
+    }
+  });
+
+  buttonLeft.addEventListener("click", function () {
+    currentStep -= 1;
+    buttonRight.style.display = "block";
+    slideWidth -= currentWidth;
+
+    carousel.style.transform = `translateX(-${slideWidth}px)`;
+
+    if (slideWidth === 0) {
+      buttonLeft.style.display = "none";
+      slideWidth = currentWidth;
+    }
+  });
+
+  /*
   buttonRight.addEventListener("click", function () {
     buttonLeft.style.display = "block";
     carousel.style.transform = `translateX(-${slideWidth}px`;
@@ -32,4 +58,5 @@ function initCarousel() {
       slideWidth = currentWidth;
     }
   });
+  */
 }
